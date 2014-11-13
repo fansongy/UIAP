c50868250d0dd8dbb744eb938da1d8e838087627
fansongy <fansongy@126.com>
2014-11-13 22:02:30 +0800

    tidy java code

--- Assets/Plugins/Android/src/com/uiap/MainActivity.java
+++ Assets/Plugins/Android/src/com/uiap/MainActivity.java
@@ -1,7 +1,6 @@
 package com.uiap;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -9,6 +8,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -17,6 +19,7 @@ import com.uiap.util.IabResult;
 import com.uiap.util.Inventory;
 import com.uiap.util.Purchase;
 import com.uiap.util.SkuDetails;
+import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends UnityPlayerActivity {
@@ -24,18 +27,41 @@ public class MainActivity extends UnityPlayerActivity {
 		IabHelper mHelper;
 		
 		//Google Billing	
-		String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAla7Z7tl7xGftCiJeA2rjJ1O/pfHjSb7mhHs64e0PTaPQcDsasIesf07iCSScACKtCHN8fzTW0t5eqjfgjKJkAkx6fRtizIyO09q9yDGwvQAj+lVbvjwRBG7kaI4moWMgwCFQQ6UNgjNrpO8KXojrkR3wjv72UeqIZ22F4U4+FBoIr+M2PqZ4utXpvUsh2H0zYsxj8uE771Xhemjk9jBFxnFMxWHAcSLS+rQatuS3/NohM4u2plo08xHP/nkSgLSmmBdnu4tQa4lzrsvzUI4qbFEgZbkCv+F2r4ml8Ot4NZjAvJ4JpOVj7bHhLU8Xkw3SfVGUCWOxn8iqyVpGrX50FQIDAQAB";
-		final String GOOGLE_TAG = "GoogleTag";
-		List<String> m_reqSkuList = Arrays.asList("item0","item1","item2");
+		String base64EncodedPublicKey;
+		final String GOOGLE_TAG = "GoogleBilling";
+		List<String> m_reqSkuList = new ArrayList<String>();
+		
 		String mCurPurchase = "";
 		 
 		@Override protected void onCreate (Bundle savedInstanceState)
 		{
 			super.onCreate(savedInstanceState);
+			
+			//Get ConfigData From Manifest
+			ActivityInfo info = null;
+			try {
+				info = this.getPackageManager().getActivityInfo(getComponentName(),PackageManager.GET_META_DATA);
+			} catch (NameNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String key=info.metaData.getString("PublicKey");
+			base64EncodedPublicKey = key; 
+			
+			String defaultSku=info.metaData.getString("defaultSku");
+			String[] skus = defaultSku.split(",");
+			m_reqSkuList = new ArrayList<String>();
+			for(int i = 0 ;i<skus.length;++i)
+			{
+				m_reqSkuList.add(skus[i]);
+			}
+
 			initGoogleBill();
 		}
 		
+		
 		//------------Google Billing begin----------
+		//init Google Billing and query unFinished order
 		void initGoogleBill()
 		{
 			 // compute your public key and store it in base64EncodedPublicKey
@@ -49,12 +75,100 @@ public class MainActivity extends UnityPlayerActivity {
 						 // Hooray, IAB is fully set up!
 						Log.d(GOOGLE_TAG, "Hooray, IAB is fully set up!");
 						
+						/*
+						 *  Although we should call getList() when we init game, but If we are not, and call pay() directly,
+						 * ,it will case an Error.so I add this to make sure it work.
+						 * If you want control unfinished order checking by your self,
+						 * you can delete under code,and choose right time to call getList()
+						 */   
 						mHelper.queryInventoryAsync(mQueryItems);
 				   }
 				});
 		}
 		
-		void pay(String itemSKU)
+		
+		final IabHelper.QueryInventoryFinishedListener 
+		   mQueryItems = new IabHelper.QueryInventoryFinishedListener() {
+		   public void onQueryInventoryFinished(IabResult result, Inventory inventory)   
+		   {
+		      if (result.isFailure()) {
+		         // handle error
+		         return;
+		      }
+		      
+		      //put all items of inventory which is in skuList to JSONObject
+		      JSONObject jsonResult = new JSONObject();
+		      try
+		      {     
+			      for(int i = 0; i<m_reqSkuList.size();++i)
+			      {
+			    	  String sku = m_reqSkuList.get(i);
+			    	  SkuDetails detail = inventory.getSkuDetails(sku);
+			    	  JSONObject info = fillInfo(detail);
+			    	  jsonResult.put(sku, info);
+
+			      }
+		      } catch (JSONException ex) {  
+		    	  throw new RuntimeException(ex);  
+		      }
+
+		      String resultString  = jsonResult.toString();
+		      Log.d(GOOGLE_TAG,"Inventory Result: "+ resultString);
+		      
+		      //responds the itemList data to Unity3d
+		      if(jsonResult.length() >0) 
+		      {
+		    	  UnityPlayer.UnitySendMessage("Bridge", "onGetItem", jsonResult.toString());
+		      }
+		      
+		      Log.d(GOOGLE_TAG, "check unfinished ...");
+		      
+		      //check unfinished purchase in skuList 
+		      for(int i = 0; i<m_reqSkuList.size();++i)
+		      {
+		    	  String sku = m_reqSkuList.get(i);
+		    	  Purchase pur = inventory.getPurchase(sku);
+		    	  if (pur != null && verifyDeveloperPayload(pur)) {
+	                  Log.d(GOOGLE_TAG, "find unfinished purchase, SKU: "+sku);
+	                  mCurPurchase = sku;
+	                  mHelper.consumeAsync(pur,mConsumeFinishedListener);
+	                 return;
+	              }
+		      }
+
+//		      pay("item1");
+//           String jsonStr = "{\"items\":[\"item0\",\"item1\",\"item2\"]}";
+//		      getList(jsonStr);
+		   }
+		};
+		
+		
+		public void getList(String jsonStr)
+		{		
+			try
+			{
+				if(jsonStr.length() > 0)
+				{
+					JSONObject json = new JSONObject(jsonStr);
+				  	JSONArray list = json.getJSONArray("items");
+				  	m_reqSkuList = new ArrayList<String>();
+				  	for(int i = 0 ;i<list.length();++i)
+				  	{
+				  		m_reqSkuList.add(list.getString(i));
+				  	}
+				}
+				mHelper.queryInventoryAsync(true, m_reqSkuList,
+						 mQueryItems);
+
+			}
+			 catch (JSONException ex) 
+			 { 
+				throw new RuntimeException(ex); 
+			 }
+			
+		}
+	
+		public void pay(String itemSKU)
 		{
 			mCurPurchase = itemSKU;
 			mHelper.launchPurchaseFlow(this, itemSKU, 10001, mPurchaseFinishedListener);
@@ -68,21 +182,69 @@ public class MainActivity extends UnityPlayerActivity {
 	            	Log.d(GOOGLE_TAG, "Error purchasing: " + result);
 	                return;
 	            }
-//	            if (!verifyDeveloperPayload(purchase)) {
-//	                cLog.d(GOOGLE_TAG, "Error purchasing. Authenticity verification failed.");
-//	                return;
-//	            }
+	            if (!verifyDeveloperPayload(purchase)) {
+	            	Log.d(GOOGLE_TAG, "Error purchasing. Authenticity verification failed.");
+	                return;
+	            }
 
 	            Log.d(GOOGLE_TAG,  "Purchase successful.");
 
-	  	      if (purchase.getSku().equals(mCurPurchase)) {	  
-	            // remove query inventory method from here and put consumeAsync() directly
-	           mHelper.consumeAsync(purchase, mConsumeFinishedListener);
-
-	  	      }
+	            /*
+	             * if your item is consume,you MUST call this.
+	             * In Google Billing,everything isn't consumed until you call consumeAsync.
+	             * Because nearly all IAP is consumed,I didn't make a data to save item kind 
+	             */
+	  	        if (purchase.getSku().equals(mCurPurchase)) {	  
+		           // remove query inventory method from here and put consumeAsync() directly
+		           mHelper.consumeAsync(purchase, mConsumeFinishedListener);
+	  	        }
 
 	        }
 	    };
+
+	    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener =
+  		    new IabHelper.OnConsumeFinishedListener() {
+  		    public void onConsumeFinished(Purchase purchase, IabResult result) {
+  		        if (result.isSuccess()) {
+  		           // provision the in-app purchase to the user
+  		           // (for example, credit 50 gold coins to player's character)
+  		           Log.d(GOOGLE_TAG,"Consume "+ mCurPurchase+" Success");
+  		           
+  		           //responds Unity3d purchase result
+  		           UnityPlayer.UnitySendMessage("Bridge", "onPay", purchase.getOrderId());
+  		           
+  		       }
+  		       else {
+  		           // handle error
+  		    	   Log.d(GOOGLE_TAG,"Consume "+ mCurPurchase+" Error");
+  		      }
+  		   }
+  		};
+	    
+		JSONObject fillInfo(SkuDetails detail)
+		{
+			JSONObject data = null;
+			try {
+				if(detail != null)
+				{
+					  String price = detail.getPrice();
+					  String desc = detail.getDescription();
+					  String title = detail.getTitle();
+					  String product = detail.getSku();
+					  
+					  data = new JSONObject();
+					  
+					  data.put("title", title);
+					  data.put("price", price);
+					  data.put("desc", desc);
+					  data.put("product", product);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return data;
+		}
 		
 	    @Override
 	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -100,98 +262,36 @@ public class MainActivity extends UnityPlayerActivity {
 		    }
 		}
 		
-		IabHelper.OnConsumeFinishedListener mConsumeFinishedListener =
-		  		   new IabHelper.OnConsumeFinishedListener() {
-		  		   public void onConsumeFinished(Purchase purchase, IabResult result) {
-		  		      if (result.isSuccess()) {
-		  		         // provision the in-app purchase to the user
-		  		         // (for example, credit 50 gold coins to player's character)
-		  		    	  Log.d(GOOGLE_TAG,"Purchase "+ mCurPurchase+" Success");
-		  		      }
-		  		      else {
-		  		         // handle error
-		  		    	  Log.d(GOOGLE_TAG,"Purchase "+ mCurPurchase+" Error");
-		  		      }
-		  		   }
-		  		};
+		
 		  		
-		void getList(String jsonStr)
-		{		
-			try
-			{
-				JSONObject json = new JSONObject(jsonStr);
-			  	JSONArray list = json.getJSONArray("items");
-			  	m_reqSkuList = new ArrayList<String>();
-			  	for(int i = 0 ;i<list.length();++i)
-			  	{
-			  		m_reqSkuList.add(list.getString(i));
-			  	}
-				 mHelper.queryInventoryAsync(true, m_reqSkuList,
-						 mQueryItems);
+  		/** Verifies the developer payload of a purchase. */
+	    boolean verifyDeveloperPayload(Purchase p) {
+	        String payload = p.getDeveloperPayload();
 
-			}
-			 catch (JSONException ex) 
-			 { 
-				throw new RuntimeException(ex); 
-			 }
-			
-		}
+	        /*
+	         * TODO: verify that the developer payload of the purchase is correct. It will be
+	         * the same one that you sent when initiating the purchase.
+	         *
+	         * WARNING: Locally generating a random string when starting a purchase and
+	         * verifying it here might seem like a good approach, but this will fail in the
+	         * case where the user purchases an item on one device and then uses your app on
+	         * a different device, because on the other device you will not have access to the
+	         * random string you originally generated.
+	         *
+	         * So a good developer payload has these characteristics:
+	         *
+	         * 1. If two different users purchase an item, the payload is different between them,
+	         *    so that one user's purchase can't be replayed to another user.
+	         *
+	         * 2. The payload must be such that you can verify it even when the app wasn't the
+	         *    one who initiated the purchase flow (so that items purchased by the user on
+	         *    one device work on other devices owned by the user).
+	         *
+	         * Using your own server to store and verify developer payloads across app
+	         * installations is recommended.
+	         */
 
-	  		
-		final IabHelper.QueryInventoryFinishedListener 
-		   mQueryItems = new IabHelper.QueryInventoryFinishedListener() {
-		   public void onQueryInventoryFinished(IabResult result, Inventory inventory)   
-		   {
-		      if (result.isFailure()) {
-		         // handle error
-		         return;
-		       }
-		      
-		      JSONObject jsonResult = new JSONObject();
-		      try
-		      {     
-			      for(int i = 0; i<m_reqSkuList.size();++i)
-			      {
-			    	  String sku = m_reqSkuList.get(i);
-			    	  SkuDetails detail = inventory.getSkuDetails(sku);
-			    	  if(detail != null)
-			    	  {
-				    	  String price = detail.getPrice();
-				    	  String desc = detail.getDescription();
-				    	  String title = detail.getTitle();
-				    	  String product = detail.getSku();
-				    	  JSONObject data = new JSONObject();
-				    	  data.put("title", title);
-				    	  data.put("price", price);
-				    	  data.put("desc", desc);
-				    	  data.put("product", product);
-				    	  jsonResult.put(sku, data);
-			    	  }
-			      }
-		      } catch (JSONException ex) {  
-		    	  throw new RuntimeException(ex);  
-		      }
-		      Log.d(GOOGLE_TAG, "check unfinished ...");
-		      Log.d(GOOGLE_TAG,jsonResult.toString());
-		      
-		      //check SKU
-		      for(int i = 0; i<m_reqSkuList.size();++i)
-		      {
-		    	  String sku = m_reqSkuList.get(i);
-		    	  Purchase pur = inventory.getPurchase(sku);
-		    	  if (pur != null){ // && verifyDeveloperPayload(pur)) {
-	                  Log.d(GOOGLE_TAG, "find unfinished purchase, SKU:"+sku);
-	                  mCurPurchase = sku;
-	                  mHelper.consumeAsync(pur,
-	                          mConsumeFinishedListener);
-	                 return;
-	              }
-		      }
-
-		      pay("item1");
-//              String jsonStr = "{\"items\":[\"item0\",\"item1\",\"item2\"]}";
-//		      getList(jsonStr);
-		   }
-		};
+	        return true;
+	    }
 	 
 }
